2b2ab002d8307485f892422784e8d5841cd6c6c4
renatopcinfo <renatopcinfo@gmail.com>
2020-08-26 00:14:34 -0300

    techs

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -31,11 +31,25 @@ fragment sectionAboutProject on LandingPage{
     }
   }
 }
+
+fragment sectionTech on LandingPage {
+  sectionTech {
+    title
+    techIcons {
+      title
+      icon {
+        url
+      }
+    }
+  }
+}
+
 query GET_LANDING_PAGE {
   landingPage {
     ...logo
     ...header
     ...sectionAboutProject
+    ...sectionTech
   }
 }
 `
